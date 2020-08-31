5e846bc8130934b7bc3f0c46f81d30a4161945c9
Nofrets Poai <opetstudio@gmail.com>
2020-08-31 11:55:37 +0700

    DEB-3 test integrate to jira

--- src/collections/toko_toko_online/services.js
+++ src/collections/toko_toko_online/services.js
@@ -99,6 +99,8 @@ const doCreateData = async (args, context) => {
     data.updated_by = userDetail._id
     data.created_at = now
     data.updated_at = now
+
+
     if (!_.isEmpty(data.name)) data.slug = (data.name || '').toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')
     const createResponse = (await EntityModel.create([data], opts))[0]
     console.log('createResponse====>', createResponse)
