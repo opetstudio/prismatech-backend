84799395c611ce85e3c94158dac5118565bcceb9
Nofrets Poai <opetstudio@gmail.com>
2020-09-01 16:12:36 +0700

    migrate lms & rp

--- schema.js
+++ schema.js
@@ -1,11 +1,43 @@
 const graphql = require('graphql')
+
+// RAYAPAY
+// Emoney
+const { allTransaction } = require('./src/collections/rp_emoney/graphql/query')
+
+// Merchant
+const { signUpMerchant, logoutMerchant, relationMerchantInstitution, changePasswordMerchant } = require('./src/collections/rp_merchant/graphql/mutation')
+const { AllMerchant, MerchantInfo, loginMerchant, MerchantTransactionHistory, merchantDashboard, showRelatedInstitution } = require('./src/collections/rp_merchant/graphql/query')
+
+// Institution
+const { logoutInstitution, signUpInstitution } = require('./src/collections/rp_institution/graphql/mutation')
+const { AllInstitution, loginInstitution, InstitutionInfo } = require('./src/collections/rp_institution/graphql/query')
+
+// Qr
+const { createQrStatic, testing, createQrDynamic } = require('./src/collections/rp_qr/graphql/mutation')
+const { showQR } = require('./src/collections/rp_qr/graphql/query')
+
+// Fee
+const { addFee, setMerchantFee, setInstitutionFee } = require('./src/collections/rp_fee/graphql/mutation')
+
+// Settlement
+const { setSettlement } = require('./src/collections/rp_settlement/graphql/mutation')
+const { getAllSettlement, getSettlements } = require('./src/collections/rp_settlement/graphql/query')
+
 // Otp
 const {
   sendOtp,
   submitOtp,
   changePasswordViaForgetPassword,
-  forgetPasswordSendOtp
+  forgetPasswordSendOtp,
+  merchantForgetPassword,
+  merchantSubmitForgetPassword,
+  institutionForgetPassword,
+  institutionSubmitForgetPassword
 } = require('./src/collections/rp_otp/graphql/mutation')
+// Services
+const { topupVa, staticQrPayment, scanQrStatic, detailPayment, cancelStaticPayment, transactionReceipt, topupInstitution, dynamicQrPayment, scanQrDynamic, createQrTopUpMerchant, scanQrTopUpMerchant, paymentTopUpMerchant } = require('./src/services/graphql/mutation')
+const { transactionHistory } = require('./src/services/graphql/query')
+
 // User
 const { signUpV2, signUp, changeUserPassword, changeUserName, changeUserProfile, logout } = require('./src/collections/user/graphql/mutation')
 const { getDetailUser, login, getProfile, allUser, getAllUsers } = require('./src/collections/user/graphql/query')
@@ -25,6 +57,32 @@ const { createPrivilege, deletePrivilege, privilegeCheckboxSubmit, updatePrivile
 const { getAllUserRoles, getDetailUserRole, getDetailUserRoleByMyUserId } = require('./src/collections/user_role/graphql/query')
 const { createUserRole, deleteUserRole, updateUserRole } = require('./src/collections/user_role/graphql/mutation')
 
+// LMS
+
+// lms course enrollment
+const { getAllEnrollmentUserByCourseId, getAllEnrollmentUserByFilter } = require('./src/collections/lms_course_enrollment/graphql/query')
+const { submitCourseEnrollmentRequest } = require('./src/collections/lms_course_enrollment/graphql/mutation')
+
+// Teacher
+const { createTeacher } = require('./src/collections/lms_teacher/graphql/mutation')
+const { getTeacherById, getAllTeachers } = require('./src/collections/lms_teacher/graphql/query')
+
+// Course
+const { createCourse, updateCourse, deleteCourse } = require('./src/collections/lms_course/graphql/mutation')
+const { getCourseById, getAllCourses, getDetailCourse, getAllPublishedCourses, getDetailPublishedCourse } = require('./src/collections/lms_course/graphql/query')
+
+// Subject
+const { createSubject, updateSubject, deleteSubject } = require('./src/collections/lms_subject/graphql/mutation')
+const { getAllSubjects, getDetailSubject } = require('./src/collections/lms_subject/graphql/query')
+
+// Subject Unit
+const { createLmsSubjectUnit, updateLmsSubjectUnit, deleteLmsSubjectUnit } = require('./src/collections/lms_subject_unit/graphql/mutation')
+const { getAllLmsSubjectUnits, getDetailLmsSubjectUnit } = require('./src/collections/lms_subject_unit/graphql/query')
+// const { getAllSettlement, getSettlements } = require('./src/collections/settlement/graphql/query')
+
+// const { tablepaginationFetchData } = require('./src/services/graphql/query')
+
+// ECOMMERCE
 // toko product
 const { getAllTokoProducts, getDetailTokoProduct, getDetailTokoProductByCode, getDetailTokoProductJoinCartByCode, getAllTokoProductsByTokoId, getAllTokoProductsByCategoryId } = require('./src/collections/toko_product/graphql/query')
 const { createTokoProduct, deleteTokoProduct, updateTokoProduct } = require('./src/collections/toko_product/graphql/mutation')
@@ -61,6 +119,55 @@ const {
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    // LMS
+    // enrollment
+    getAllEnrollmentUserByCourseId,
+    getAllEnrollmentUserByFilter,
+    // teacher
+    getTeacherById,
+    getAllTeachers,
+
+    // course
+    getCourseById,
+    getAllCourses,
+    getDetailCourse,
+    getAllPublishedCourses,
+    getDetailPublishedCourse,
+
+    // subject
+    getAllSubjects,
+    getDetailSubject,
+
+    // subject unit
+    getAllLmsSubjectUnits,
+    getDetailLmsSubjectUnit,
+
+    // RAYAPAY
+    // emoney
+    allTransaction,
+
+    // merchant
+    AllMerchant,
+    MerchantInfo,
+    loginMerchant,
+    MerchantTransactionHistory,
+    merchantDashboard,
+    showRelatedInstitution,
+
+    // institution
+    AllInstitution,
+    loginInstitution,
+    InstitutionInfo,
+
+    // qr
+    showQR,
+
+    // settlement
+    getAllSettlement,
+    getSettlements,
+
+    // services
+    transactionHistory,
 
     // user
     login,
@@ -121,11 +228,77 @@ const RootQuery = new GraphQLObjectType({
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
+
+    // LMS
+    // enrollment
+    submitCourseEnrollmentRequest,
+    // teacher
+    createTeacher,
+
+    // course
+    createCourse,
+    updateCourse,
+    deleteCourse,
+
+    // subject
+    createSubject,
+    updateSubject,
+    deleteSubject,
+
+    // subject unit
+    createLmsSubjectUnit,
+    updateLmsSubjectUnit,
+    deleteLmsSubjectUnit,
+
+    // RAYAPAY
+
+    // merchant
+    signUpMerchant,
+    logoutMerchant,
+    relationMerchantInstitution,
+    changePasswordMerchant,
+
+    // institution
+    logoutInstitution,
+    signUpInstitution,
+
+    // qr
+    createQrStatic,
+    testing,
+    createQrDynamic,
+
+    // fee
+    addFee,
+    setMerchantFee,
+    setInstitutionFee,
+
+    // settlement
+    setSettlement,
+
     // otp
     sendOtp,
     submitOtp,
     changePasswordViaForgetPassword,
     forgetPasswordSendOtp,
+    merchantForgetPassword,
+    merchantSubmitForgetPassword,
+    institutionForgetPassword,
+    institutionSubmitForgetPassword,
+
+    // services
+    topupVa,
+    staticQrPayment,
+    scanQrStatic,
+    detailPayment,
+    cancelStaticPayment,
+    transactionReceipt,
+    topupInstitution,
+    dynamicQrPayment,
+    scanQrDynamic,
+    createQrTopUpMerchant,
+    scanQrTopUpMerchant,
+    paymentTopUpMerchant,
+
     // user
     signUp,
     signUpV2,
@@ -155,6 +328,7 @@ const Mutation = new GraphQLObjectType({
     updateUserRole,
     deleteUserRole,
 
+    // toko product
     createTokoProduct,
     deleteTokoProduct,
     updateTokoProduct,
