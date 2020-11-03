9a35043d357226875484a16a175b2646e0b6d648
Nofrets Poai <opetstudio@gmail.com>
2020-11-03 10:34:58 +0700

    deploy sdk

--- javascript-sdk/product_catalog.js
+++ javascript-sdk/product_catalog.js
@@ -20,7 +20,8 @@ var _MaterialUI = MaterialUI,
     CardActionArea = _MaterialUI.CardActionArea,
     CircularProgress = _MaterialUI.CircularProgress,
     IconButton = _MaterialUI.IconButton,
-    SvgIcon = _MaterialUI.SvgIcon;
+    SvgIcon = _MaterialUI.SvgIcon,
+    Chip = _MaterialUI.Chip;
 
 
 var backendBaseUrl = TOKOONLINE_BASEURL;
@@ -39,7 +40,7 @@ var useStyles = makeStyles(function (theme) {
             marginTop: theme.spacing(4)
         },
         mkPlinkCatCardGrid: {
-            padding: theme.spacing(4)
+            // padding: theme.spacing(4)
         },
         mkPlinkCatCard: {
             height: '100%',
@@ -86,19 +87,19 @@ var useStyles = makeStyles(function (theme) {
             zIndex: 3
         },
         mkPlinkCatgToolbarPrimaryBox: {
-            backgroundColor: theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800]
+            // backgroundColor:
+            //     theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800]
         },
         mkPlinkCatgToolbarPrimaryBoxGrid: {
-            marginTop: theme.spacing(3)
+            // marginTop: theme.spacing(3),
         },
         chip: {
             margin: theme.spacing(0.5)
         }
     };
 });
-// const tokoonline_category_style = parseInt(document.getElementById('tokoonline_category').dataset.catgtype);
 var fabPosition = document.getElementById('tokoonline_content').dataset.fabposition;
-// class App extends React.Component {
+
 function App() {
     var tokoonlinesessionid = localStorage.getItem(TOKOONLINE_TOKOID);
     if (!tokoonlinesessionid) localStorage.setItem(TOKOONLINE_TOKOID, '' + new Date().getTime());
@@ -150,7 +151,7 @@ function App() {
 
         //     // fetch product
         // Simple POST request with a JSON body using fetch
-        var graphqlData = 'query{\n      getAllTokoProductsByTokoId(\n        toko_id: "' + TOKOONLINE_TOKOID + '",\n        ' + filterByCategory + '\n        page_size: 9, \n        page_index: ' + page + ', \n        string_to_search: ""\n       ){\n        error,\n        count,\n        page_count,\n        status,\n        list_data{\n          _id,\n          name,\n          price,\n          code,\n          description,\n          category_id{\n            _id,\n            title,   \n          },\n          image_id{\n            _id,\n            filename,\n            file_type\n          }\n        }\n      }\n    }';
+        var graphqlData = 'query{\n      getAllTokoProductsByTokoId(\n        toko_id: "' + TOKOONLINE_TOKOID + '",\n        ' + filterByCategory + '\n        page_size: 9, \n        page_index: ' + page + ', \n        string_to_search: ""\n       ){\n        error,\n        count,\n        page_count,\n        status,\n        list_data{\n          _id,\n          name,\n          price,\n          code,\n          instock_label,\n          preorder_policy,\n          stock_amount,\n          description,\n          category_id{\n            _id,\n            title,   \n          },\n          image_id{\n            _id,\n            filename,\n            file_type\n          }\n        }\n      }\n    }';
         var requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -273,22 +274,25 @@ function App() {
                 {
                     container: true,
                     direction: 'row',
-                    justify: fabPosition == 'left' ? "flex-start" : 'flex-end',
-                    alignItems: 'flex-end',
-                    className: classes.mkPlinkCatFabCart
+                    justify: 'space-between',
+                    alignItems: 'center'
                 },
                 React.createElement(
+                    'h1',
+                    { style: { color: '#4B8BE2' } },
+                    'Catalog'
+                ),
+                React.createElement(
                     Badge,
-                    { onClick: goToCart, badgeContent: 0, color: 'secondary' },
+                    { style: { marginRight: 11 }, onClick: goToCart, badgeContent: 0, color: 'secondary' },
                     React.createElement(
                         IconButton,
                         { edge: 'end', 'aria-label': 'delete',
-                            style: { marginTop: -2, marginRight: -5, backgroundColor: '#efefef' } },
+                            style: { backgroundColor: '#4B8BE2' } },
                         React.createElement(
                             SvgIcon,
-                            null,
-                            React.createElement('path', {
-                                d: 'M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z' })
+                            { viewBox: "-3 0 512 512" },
+                            React.createElement('path', { fill: '#fff', d: 'm147.925 379.116c-22.357-1.142-21.936-32.588-.001-33.68 62.135.216 226.021.058 290.132.103 17.535 0 32.537-11.933 36.481-29.017l36.404-157.641c2.085-9.026-.019-18.368-5.771-25.629s-14.363-11.484-23.626-11.484c-25.791 0-244.716-.991-356.849-1.438l-17.775-65.953c-4.267-15.761-18.65-26.768-34.978-26.768h-56.942c-8.284 0-15 6.716-15 15s6.716 15 15 15h56.942c2.811 0 5.286 1.895 6.017 4.592l68.265 253.276c-12.003.436-23.183 5.318-31.661 13.92-8.908 9.04-13.692 21.006-13.471 33.695.442 25.377 21.451 46.023 46.833 46.023h21.872c-3.251 6.824-5.076 14.453-5.076 22.501 0 28.95 23.552 52.502 52.502 52.502s52.502-23.552 52.502-52.502c0-8.049-1.826-15.677-5.077-22.501h94.716c-3.248 6.822-5.073 14.447-5.073 22.493 0 28.95 23.553 52.502 52.502 52.502 28.95 0 52.503-23.553 52.503-52.502 0-8.359-1.974-16.263-5.464-23.285 5.936-1.999 10.216-7.598 10.216-14.207 0-8.284-6.716-15-15-15zm91.799 52.501c0 12.408-10.094 22.502-22.502 22.502s-22.502-10.094-22.502-22.502c0-12.401 10.084-22.491 22.483-22.501h.038c12.399.01 22.483 10.1 22.483 22.501zm167.07 22.494c-12.407 0-22.502-10.095-22.502-22.502 0-12.285 9.898-22.296 22.137-22.493h.731c12.24.197 22.138 10.208 22.138 22.493-.001 12.407-10.096 22.502-22.504 22.502zm74.86-302.233c.089.112.076.165.057.251l-15.339 66.425h-51.942l8.845-67.023 58.149.234c.089.002.142.002.23.113zm-154.645 163.66v-66.984h53.202l-8.84 66.984zm-74.382 0-8.912-66.984h53.294v66.984zm-69.053 0h-.047c-3.656-.001-6.877-2.467-7.828-5.98l-16.442-61.004h54.193l8.912 66.984zm56.149-96.983-9.021-67.799 66.306.267v67.532zm87.286 0v-67.411l66.022.266-8.861 67.145zm-126.588-67.922 9.037 67.921h-58.287l-18.38-68.194zm237.635 164.905h-36.426l8.84-66.984h48.973l-14.137 61.217c-.784 3.396-3.765 5.767-7.25 5.767z' })
                         )
                     )
                 )
@@ -307,7 +311,7 @@ function App() {
                 Grid,
                 { container: true, spacing: 2 },
                 (listData || []).map(function (v, i) {
-                    return React.createElement(
+                    return v.preorder_policy !== 'unavailable' ? React.createElement(
                         Grid,
                         { item: true, key: i, xs: 6, sm: 6, md: 6, lg: 4 },
                         React.createElement(
@@ -323,6 +327,7 @@ function App() {
                                     image: backendBaseUrl + '/renderfile/' + (v.image_id || {}).filename + '.' + (v.image_id || {}).file_type,
                                     title: v.name
                                 }),
+                                React.createElement(Chip, { style: { marginTop: 10, marginLeft: 16 }, size: 'small', color: 'primary', label: v.stock_amount == 0 && v.preorder_policy == "preorder_policy" ? "Pre-Order" : v.instock_label }),
                                 React.createElement(
                                     CardContent,
                                     { className: classes.mkPlinkCatCardContent },
@@ -354,7 +359,7 @@ function App() {
                                 )
                             )
                         )
-                    );
+                    ) : "";
                 })
             ),
             React.createElement(
